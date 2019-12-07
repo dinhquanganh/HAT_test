c5d8832490286ac53b2dc31d9be24faeb76d300b
ntttrung1210 <56528546+ntttrung1210@users.noreply.github.com>
2019-12-07 14:33:33 +0700

    done

--- crawlernow.py
+++ crawlernow.py
@@ -4,8 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 from hfdb import *
 browser=webdriver.Chrome()
-browser.get('https://www.now.vn/ha-noi/danh-sach-dia-diem-giao-tan-noi?q=c%C6%A1m%20rang')
-time.sleep(2)
+browser.get('https://www.now.vn/ha-noi/danh-sach-dia-diem-giao-tan-noi?q=ch%C3%A8')
 ls=[]
 for i in range(3):
     ele=browser.find_elements_by_xpath("//div[@class='item-restaurant']")
@@ -38,5 +37,5 @@ for i in range(3):
             # ls.append({'duong':duong,'name':name,'link':link})
     ele1=browser.find_element_by_xpath("//span[@class='icon icon-paging-next']")
     ele1.click()
-    time.sleep(1)
+    time.sleep(2)
 browser.close()
